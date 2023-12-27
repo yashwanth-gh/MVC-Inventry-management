d3a3b5c1141ee6e63ec296c04166761d4210e88f
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-27 18:56:52 +0530

    created model

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,8 @@
 import path from 'path';
-
+import ProductModel from '../models/product.model.js';
 export default class ProductController{
     getProducts(req,res){
+        console.log(ProductModel.get());
         return res.sendFile(path.join(path.resolve(),"src","views","products.html"))
     }
 }
